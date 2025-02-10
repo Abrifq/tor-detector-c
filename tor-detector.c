6246651a83ced3ec194fea7d57e977d4907ec40f
Abrifq <melezorus34@gmail.com>
2025-02-11 02:08:00 +0300

    Fixed buffer errors + satisfied linters

--- tor-detector.c
+++ tor-detector.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,30 +7,32 @@
 #include "tor-detector.h"
 #define DNS_QUERY_SUFFIX ".dnsel.torproject.org"
 
-void reverseIP(const char *ip, char **dest)
+void reverseIP(const char* ip, char** dest)
 {
     int part1, part2, part3, part4;
-    char *buf = malloc(strlen(ip));
+    size_t strLen = strlen(ip) + 1;
+    char* buf = (char*)malloc(strLen);
     sscanf(ip, "%d.%d.%d.%d", &part1, &part2, &part3, &part4);
-    sprintf(buf, "%d.%d.%d.%d", part4, part3, part2, part1);
+    snprintf(buf, strLen, "%d.%d.%d.%d", part4, part3, part2, part1);
     *dest = buf;
     return;
 }
 
-int isTorExitNode(const char *ip)
+int isTorExitNode(const char* ip)
 {
 
-    char *reversedIP;
+    char* reversedIP;
     reverseIP(ip, &reversedIP);
-    char *node = malloc(strlen(reversedIP) + sizeof(DNS_QUERY_SUFFIX));
-    sprintf(node, "%s" DNS_QUERY_SUFFIX,
-            reversedIP);
-    struct addrinfo *results;
+    size_t dnsQueryAddressLength = strlen(reversedIP) + sizeof(DNS_QUERY_SUFFIX); //sizeof already includes the null byte, we don't need to add it by hand.
+    char* node = (char*)malloc(dnsQueryAddressLength);
+    snprintf(node, dnsQueryAddressLength, "%s%s", reversedIP, DNS_QUERY_SUFFIX);
+    struct addrinfo* results = NULL;
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     int queryResult = getaddrinfo(node, NULL, &hints, &results);
-    freeaddrinfo(results);
+    if (results != NULL)
+        freeaddrinfo(results);
     free(reversedIP);
     free(node);
     return queryResult ? 0 : 1;
